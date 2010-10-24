f4422d68c56de27aa926cb167f2075d87a0bf8ae
Juha Arpiainen <jarpiain@iki.fi>
2010-10-24 18:54:06 +0300

    Created a lein project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject cljc "0.1.0-SNAPSHOT"
+  :description "A clojure port of the clojure compiler"
+  :url "http://github.com/jarpiain/cljc"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
